aed24d6e524a27c210f986218b619519dc02ebf1
Jim Brancheau <jim.brancheau@gmail.com>
2023-12-07 00:23:56 -0500

    use headless firefox

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 require 'capybara/cucumber'
+require 'selenium/webdriver'
 
-Capybara.register_driver :selenium do |app|
-  browser_options = ::Selenium::WebDriver::Firefox::Options.new
-  browser_options.args << '--headless'
-  Capybara::Selenium :Driver.new(app, browser: firefox, options: browser_options)
+driver = ENV['DRIVER'] || 'selenium'
+
+Capybara.register_driver :headless_firefox do |app|
+  options = Selenium::WebDriver::Firefox::Options.new
+  options.headless!
+  Capybara::Selenium::Driver.new(app, browser: :firefox, options:)
 end
+
 Capybara.run_server = false
 # Set default driver to Selenium
-Capybara.default_driver = :selenium
+Capybara.default_driver = driver.to_sym
 # Set default selector strategy to css
 Capybara.default_selector = :css
