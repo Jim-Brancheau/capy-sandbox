a4d0dfa200e84a6b8b4a824bde927804b279ab80
Jim Brancheau <jim.brancheau@gmail.com>
2023-12-07 00:10:25 -0500

    set firefox to run headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,10 +2,13 @@
 
 require 'capybara/cucumber'
 
+Capybara.register_driver :selenium do |app|
+  browser_options = ::Selenium::WebDriver::Firefox::Options.new
+  browser_options.args << '--headless'
+  Capybara::Selenium :Driver.new(app, browser: firefox, options: browser_options)
+end
 Capybara.run_server = false
 # Set default driver to Selenium
 Capybara.default_driver = :selenium
 # Set default selector strategy to css
 Capybara.default_selector = :css
-
-# TODO: Need to tear down state between test runs as it stays logged in between tests
